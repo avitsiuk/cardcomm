16d11c3fd0058cc45769c4a6622209419194cb7b
a.vitsiuk <a.vitsiuk@affidaty.io>
2023-06-25 15:21:56 +0200

    changed exports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-import * as Utils from './utils'
-import CommandApdu from './commandApdu';
+import * as Tlv from './tlv';
+import * as Utils from './utils';
 import statusDecode from './statusDecode';
 import ResponseApdu from './responseApdu';
+import CommandApdu from './commandApdu';
 // import * as TypesInternal from './typesInternal';
 import Card from './card';
-// import Iso7816Application from './iso7816Application';
+import  * as Iso7816Commands from './iso7816/commands';
+
+import * as GPCommands from './globalPlatform/commands';
+import GPSecureSession from './globalPlatform/secureSession';
+
 import Device from './device';
 import Devices from './devices';
 
 export {
+    Tlv,
     Utils,
-    CommandApdu,
     statusDecode,
     ResponseApdu,
+    CommandApdu,
     Card,
-    // Iso7816Application,
+    Iso7816Commands,
+    GPCommands,
+    GPSecureSession,
     Device,
     Devices,
 };
