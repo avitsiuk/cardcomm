25d287dcf96759b8303286970eed3aa4c059c354
a.vitsiuk <a.vitsiuk@affidaty.io>
2023-06-25 15:29:30 +0200

    removed unnecesary file, added secure session to demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,16 +1,11 @@
 
-// const arr = [0x90, 0x00];
-// console.log(arr);
-// console.log()
 import {
+    // CommandApdu,
     Devices,
-    CommandApdu,
-    ResponseApdu,
-    Utils,
+    // Iso7816Commands,
+    // Utils,
+    GPSecureSession,
 } from '../src/index';
-import { arrayToHex } from '../src/utils';
-
-
 
 const pcscDevices = new Devices();
 
@@ -31,6 +26,8 @@ pcscDevices.on('device-activated', (event => {
     }
     console.log(`New device: ["${device.name}"](${devType})`);
 
+    // if (devType != devTypes.nfc) return;
+
     device.on('error', (error) => {
         console.error(`[${devType}] error: ${error.message}`);
     })
@@ -50,7 +47,8 @@ pcscDevices.on('device-activated', (event => {
             return;
         }
 
-        let card = event.card.setAutoGetResponse();
+        let card = event.card;
+        card.setAutoGetResponse();
         console.log();
         console.log(`[${devType}]: Inserted ${devType === devTypes.nfc ? 'ATS' : 'ATR'}:[${card.atrHex}]`);
 
@@ -62,42 +60,19 @@ pcscDevices.on('device-activated', (event => {
             console.log(`[${devType}]: RSP: [${response}](${response.meaning()})`);
         });
 
-        // let cmd = new CommandApdu('00a4040000'); //.setData(Utils.hexToArray('112233445500'));
-        // console.log(`CMD:  [${cmd}]`);
-        // console.log(`Lc:   [${cmd.getLc().toString(16).padStart(2, '0')}](${cmd.getLc()})`);
-        // console.log(`Data: [${Utils.arrayToHex(cmd.getData())}]`);
-        // console.log(`le: [${cmd.getLe().toString(16)}](${cmd.getLe()})`);
-        await card.issueCommand(new CommandApdu().setIns(0xA4).setP1(0x04));
-        console.log('=======');
-        await card.issueCommand(
-            new CommandApdu().setIns(0xA4).setP1(0x04).setData(Utils.hexToArray('112233445500')),
-        );
-        console.log('=======');
-        card.issueCommand(new CommandApdu('80ff000000'), (err, res) => {
-            console.log(`Final response: [${res.toString()}]`);
-        });
-        // console.log('');
-        // console.log(res.toString());
+        const secSession = new GPSecureSession(card);
 
-
-
-        // await card.issueCommand('00A4040000');
-        // await card.issueCommand('00A404000611223344550000');
-        // ("80ca006600"
-
-        // await card.issueCommand('00a404000611223344550000');
-        // await card.issueCommand('8000000001FF00'); // 9000
-        // await card.issueCommand('8001000001FF00'); // 9001
-        // await card.issueCommand('8002000001FF00'); // 6d00 (INS not supported)
-        // await card.issueCommand('80FF000001FF00'); // 90ff
-        // const app = new Iso7816Application(card);
-
-        // if (devType === devTypes.cnt) {
-        //     card.issueCommand('00A404000E315041592E5359532E444446303100');
-        //     // app.selectFile([0x31, 0x50, 0x41, 0x59, 0x2E, 0x53, 0x59, 0x53, 0x2E, 0x44, 0x44, 0x46, 0x30, 0x31])
-        // } else if (devType === devTypes.nfc) {
-        //     card.issueCommand('00A404000E325041592E5359532E444446303100');
-        //     // app.selectFile([0x32, 0x50, 0x41, 0x59, 0x2E, 0x53, 0x59, 0x53, 0x2E, 0x44, 0x44, 0x46, 0x30, 0x31])
-        // }
+        secSession.init()
+            .then((resp) => {
+                console.log('===================================');
+                console.log(`Authenticated to ISD!!`);
+                console.log('===================================');
+            })
+            .catch((err) => {
+                console.log('===================================');
+                console.log(`Error: ${err}`);
+                console.log('===================================');
+            })
+        // secSession.issueCommand(cmd);
     });
 }));
