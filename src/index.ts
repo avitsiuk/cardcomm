775bcacefc85ca109c572988ea8dbca15d4b4818
a.vitsiuk <a.vitsiuk@affidaty.io>
2024-09-18 17:39:53 +0200

    gathered GP and Iso7816 exports under respective namespaces

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,33 @@ import * as Utils from './utils';
 import statusDecode from './statusDecode';
 import ResponseApdu from './responseApdu';
 import CommandApdu from './commandApdu';
-// import * as TypesInternal from './typesInternal';
 import Card from './card';
 
+// Iso7816
+import * as Iso7816Values from './iso7816/values';
 import * as Iso7816Commands from './iso7816/commands';
 
+/** Everything Iso7816 */
+const Iso7816 = {
+    /** Common Iso7816 values */
+    values: Iso7816Values,
+    /** Iso7816-defined Commands */
+    commands: Iso7816Commands,
+}
+
+// GlobalPlatform
+import * as GPValues from './globalPlatform/values';
 import * as GPCommands from './globalPlatform/commands';
-import SCP02, {
-    defaultStaticKeys as gpDefaultStaticKeys,
-} from './globalPlatform/sesureSession/scp02';
+import SCP02 from './globalPlatform/sesureSession/scp02';
 import SCP11 from './globalPlatform/sesureSession/scp11';
+const GP = {
+    /** Common GlobalPlatform values */
+    values: GPValues,
+    /** GlobalPlatform-defined Commands */
+    commands: GPCommands,
+    SCP02,
+    SCP11
+}
 
 import Device from './device';
 import PcscDevicesManager from './devices';
@@ -23,12 +40,9 @@ export {
     statusDecode,
     ResponseApdu,
     CommandApdu,
+    Iso7816,
+    GP,
     Card,
-    Iso7816Commands,
-    GPCommands,
-    SCP02,
-    SCP11,
-    gpDefaultStaticKeys,
     Device,
     PcscDevicesManager,
 };
